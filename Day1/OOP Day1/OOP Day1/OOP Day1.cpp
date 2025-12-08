40b21e90fceea7cbf0b7bf22308484c7674d601f
salah3mer110 <salahamer3040@gmail.com>
2025-12-08 21:44:06 +0200

    Day 1 OOP & programming Fundamentals commit

---
+++ Day1/OOP Day1/OOP Day1/OOP Day1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+//1. Write a program that prints your name, age, and city using std::cout.
+	
+	//string name,city;
+	//int age;
+	//cout << "Enter ur name: " << endl;
+	//cin >> name;
+	//cout << "Enter ur age: " << endl;
+	//cin >> age;
+	//cout << "Enter ur City name: " << endl;
+	//cin >> city;
+	//cout << "Ur name is " << name << " with age " << age <<  " living in " << city << " city" << endl;
+	//
+	
+	//2. Write a program that reads two numbers and displays their sum, difference, and average
+	
+	//int x, y;
+	//cout << "Enter 2 nums:" << endl;
+	//cin >> x;
+	//cin >> y;
+	//cout << "Sum = " << x + y << "\n";
+	//cout << "difference = " << x - y << "\n";
+	//cout << "Avg = " << (x + y) / 2 << "\n";
+	//
+	//
+//3. Write a program that takes a decimal number and displays its hexadecimal and octal values.
+	
+	//float x;
+	//cout << "Enter a decimal number:\n";
+	//cin >> x;
+	//cout << "Hexadecimal = " << hexfloat << x << endl;
+	//cout << "octal = " << oct << x << endl;
+	//
+
+//4. Write a program that reads a character and displays its ASCII.
+	char c;
+	cout << "Enter char: \n";
+	cin >> c;
+	cout << "ASCII = " << static_cast<int>(c) << endl;
+
+	return 0;
+}
+
+
+
+/*
+*/
