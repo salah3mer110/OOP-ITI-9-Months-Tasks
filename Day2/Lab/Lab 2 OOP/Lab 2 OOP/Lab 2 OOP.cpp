d52763a9c313a34f1385a01ce98c98934f19bb9a
salah3mer110 <salahamer3040@gmail.com>
2025-12-12 17:52:55 +0200

    Day 2 OOP commit

---
+++ Day2/Lab/Lab 2 OOP/Lab 2 OOP/Lab 2 OOP.cpp
@@ -0,0 +1,153 @@
+// Lab 2 OOP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    ///////////////////////////////////////
+    //1
+
+    //int i = 1;
+    //while (i <= 100) {
+    //    if (i % 2 == 0)
+    //        cout << i << endl;
+    //    i++;
+    //}
+    
+    //---------------------------    
+    //int x;
+    //do {
+    //    cout << "Enter matrix dimension it should be an odd and greater than zero eg:3,5,7: " << endl;
+    //    cin >> x;
+    //} while (x % 2 == 0);
+    //system("cls");
+    //int row,col;
+    //row = 1;
+    //col = (x + 1) / 2;
+    //cout << "\033[" << row << ";" << col*4 << "H" << "\033[0;" << 31+(1%7) << "m" << "1"<<"\033[0m";
+    //for (int i = 2; i <= x*x; i++)
+    //{
+    //    if ((i - 1) % x != 0) {
+    //        row--;
+    //        col--;
+    //        if (row < 1)
+    //            row = x;
+    //        if (col < 1)
+    //            col = x;
+    //    }
+    //    else
+    //        row++;
+    //    cout << "\033[" << row << ";" << col * 4 << "H"<< "\033[" << 31 + (i % 7) << "m"<<i<<"\033[0m";
+    //}
+    // ---------------------------------------------------------------
+
+    //for (int i = 1; i <=x ; i++)
+    //{
+    //    for (int j = 1; j <= x; j++)
+    //    {
+    //        if (i == x / 2 + 1)
+    //            cout << "\033[" << i << ";" << j << "H"<<"\033[0;"<<i*7<<"m"<< "1";
+    //        else
+    //            cout << "\033[" << i << ";" << j << "H" << "\033[0;" << i * 7 << "m" << i;
+    //    }
+    //}
+
+    //------------------------------------------------------------------------------------------
+    //int sum = 0, n;
+    //while (sum <= 100) {
+    //    cout << "Enter num for sum" << endl;
+    //    cin >> n;
+    //    sum += n;
+    //}
+    //cout <<"Sum of nums u entered is: " << sum << endl;
+    //
+    
+    
+   //----------------------------------------------------------------------------------------------
+    //2
+
+
+
+    //int x, y; 
+    //char opChar;
+    //do {
+    //cout << "Enter 2 nums to apply operation" << endl;
+    //cin >> x;
+    //cin >> y;
+    //cout << "choose from the menu the letter of operation u want to apply \n\na.sum\nb.difference\nc.product\nd.division\n\e.exit\n";
+    //cin >> opChar;
+    //switch (opChar) {
+    //case 'a':
+    //    cout << "Result is: " << x + y << endl;
+    //    break;
+    //case 'b':
+    //    cout << "Result is: " << x - y << endl;
+    //    break;
+    //case 'c':
+    //    cout << "Result is: " << x * y << endl;
+    //    break;
+    //case 'd':
+    // if(y==0){
+    //    cout << "Invalid operation Re-Enter the second Num: ";
+    //    cin>>y;
+    // }
+    //    cout << "Result is: " << x / y << endl;
+    //    break;
+    //default:
+    //    cout << "unkown operation\n";
+    //}
+    //} while (opChar!='e');
+    //
+    
+
+
+    //---------------------------------------------------------------------------------------------
+    //4
+    
+
+
+    //int deg;
+    //cout << "Enter your degree to display grade: " << endl;
+    //cin >> deg;
+    //if (deg <= 100 && deg >= 85)
+    //    cout << "Grade A" << endl;
+    //else if (deg <= 84 && deg >= 75)
+    //    cout << "Grade B" << endl;
+    //else if (deg <= 74 && deg >= 60)
+    //    cout << "Grade C" << endl;
+    //else 
+    //    if (deg <60)
+    //    cout << "Grade F" << endl;
+    //    else
+    //    cout << "Extra ordinary Grade" << endl;
+
+    //-----------------------------------------------------------------------------------------------
+    //for (int i = 1; i <= 3; i++)
+    //{
+    //    for (int j = 1; j <= 3; j++)
+    //    {
+    //        if (i == j) 
+    //            cout << "\033[" << i << ";" << j << "H"<<"*";
+    //        else 
+    //            cout << "\033[" << i << ";" << j << "H"<<"-";
+    //    }
+    //}
+    //cout << "*";
+    //cout << "\033[1;2H" << "-";
+    //cout << "\033[1;3H" << "-";
+    //cout << "\033[2;1H" << "-";
+    //cout << "\033[2;2H" << "*";
+    //cout << "\033[2;3H" << "-";
+    //cout << "\033[3;1H" << "-";
+    //cout << "\033[3;2H" << "-";
+    //cout << "\033[3;3H" << "*";
+    /*cout << "*";
+    cout << "*";
+    cout << "*";
+    cout << "*";
+    cout << "*";
+    cout << "*";*/
+}
